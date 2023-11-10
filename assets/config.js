b6683ff0e2f672e16652beb7bb8218d28e43cbe3
aggit7 <142899804+aggit7@users.noreply.github.com>
2023-11-10 23:32:59 +0000

    init

---
+++ assets/config.js
@@ -0,0 +1,28 @@
+let config = {
+  "Title": "Daniel Gigis - Gigi's",
+  "Name": "Daniel from Gigi's",
+  "Description": "Gigi's website is currently down for maintenance. We've been getting a lot of angry bots that don't like pizza. :) Apologies for the inconvenience. Here are the links. ",
+  "links": [
+    {
+      "Title": "Order Online Now",
+      "URL": "https://www.restaurantlogin.com/ordering/restaurant/menu?company_uid=bc35d85e-c3e4-4e76-8eed-21c7f1ee9b6a&restaurant_uid=6791b679-6d4c-4430-bf5f-38699fe894e7&facebook=true",
+      "icon_classes": "fas fa-",
+    },
+    {
+      "Title": "Reserve a Table",
+      "URL": "https://tableagent.com/sydney/gigis-balmain/",
+      "icon_classes": "fab fa-agent",
+    },
+    {
+      "Title": "Main Vegan Menu",
+      "URL": "https://gigisbalmain.com.au/menu/GIGI_vegan_menu.pdf",
+      "icon_classes": "fab fa-"
+    },
+    {
+      "Title": "Main Menu",
+      "URL": "https://gigisbalmain.com.au/menu/GIGI_main.pdf",
+      "icon_classes": "fab fa-"
+    }
+    
+  ]
+}
